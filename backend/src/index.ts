9714faec7758651bc005f2b031d7d10c80ec1f84
office-diet <kw278u@bma.biglobe.ne.jp>
2026-09-22 18:33:22 +0900

    gemini api start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,61 @@
 import express from "express";
 import cors from "cors";
 import { Client } from "pg";
+import dotenv from "dotenv";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
+
+const genAI = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
+const chatHistory = [];
 
 const app = express();
 const port = 3000;
 app.use(cors());
+app.use(express.json());
 
 const client = new Client({
   connectionString: process.env.DB_URL
 });
-
 client.connect();
 
-app.get("/api/hello", async (_req, res) => {
+app.get("/api/hello", async (req, res) => {
   const result = await client.query("SELECT NOW()");
   res.json({ message: "Hello from backend!", db_time: result.rows[0].now });
 });
 
-app.get("/api/aimembers", async (_req, res) => {
-  try {
-    const result = await client.query(
-      "SELECT id, name, personality, created_at FROM aimember ORDER BY created_at ASC"
-    );
+app.get("/api/aimembers", async (req, res) => {
+  const members = await client.query("SELECT name, personality FROM aimember");
+  const aryMembers = members.rows;
+  res.json({
+    count: aryMembers.length,
+    members: aryMembers,
+  });
+});
 
-    res.json({
-      count: result.rows.length,
-      members: result.rows,
+app.post("/api/chat", async (req, res) => {
+  const userText = req.body.text;
+
+  const members = await client.query("SELECT name, personality FROM aimember");
+  const aryMembers = members.rows;
+  const randomMember = aryMembers[0];
+  const memberName = randomMember.name;
+  const personality = `my name is 【${memberName}】.` + randomMember.personality;
+  try {
+    chatHistory.push({role:"user", parts:[{text: userText}]});
+    const result = await genAI.models.generateContent({
+      model: 'gemini-3.5-flash-lite', 
+      contents: chatHistory,
+      config: {
+        systemInstruction: personality
+      }
     });
-  } catch (error) {
-    console.error("Error fetching aimembers:", error);
-    res.status(500).json({ error: "Failed to fetch AI members" });
+    chatHistory.push({role:"model", parts:[{text: userText}]});
+    res.json({ name: memberName, reply: result.text });
+
+  } catch (err) {
+    console.error(err);
+    res.json({ reply: "ごめんね、ちょっと考えすぎてしまったみたい…" });
   }
 });
 
